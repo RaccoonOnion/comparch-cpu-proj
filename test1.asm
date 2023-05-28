5fe9dba6396df681f6eae376593de4c37629a149
c82531 <115882502+c82531@users.noreply.github.com>
2023-05-28 22:17:08 +0800

    Add files via upload

--- test1.asm
+++ test1.asm
@@ -25,30 +25,31 @@ case1:
 	lw $3, 0x8C60($0)  #a
 	addi $4, $3, -1
 	and $5, $3, $4
-	sw $3, 0xBC70($0) # show a
+	sw $3, 0xDC70($0) # show a
 	bne $5, $0, control
 	addi $6, $0, 1
-	sw $6, 0xDC70($0) #light
+	sw $6, 0xEC70($0) #light
 	j control
 case2:
 	lw $3, 0x8C60($0)  #a
 	andi $4, $3, 1
-	sw $3, 0xBC70($0) # show a
+	sw $3, 0xDC70($0) # show a
 	beq $4, $0, control
 	addi $6, $0, 1
-	sw $6, 0xC70($0)  #light
+	sw $6, 0xEC70($0)  #light
 	j control
 case3:
 	or $12, $10, $11
-	sw $12, 0xDC70($0) #show
+	sw $12, 0xEC70($0) #show
 	j control
 case4:
 	nor $12, $10, $11
-	sw $12, 0xDC70($0) #show
+	andi $12, $12, 15
+	sw $12, 0xEC70($0) #show
 	j control
 case5:
 	xor $12, $10, $11
-	sw $12, 0xDC70($0) #show
+	sw $12, 0xEC70($0) #show
 	j control
 case6:
 	add $a1, $0, $10
@@ -58,18 +59,18 @@ case6:
 	jal signTurn
 	add $11, $0, $v0
 	slt $12, $10, $11
-	sw $12, 0xDC70($0) #show
+	sw $12, 0xEC70($0) #show
 	j control
 case7:	
 	sltu $12, $10, $11
-	sw $12, 0xDC70($0) #show
+	sw $12, 0xEC70($0) #show
 	j control
 case8:
 	lw $10, 0x8C60($0)  #a
 	lw $11, 0x9C60($0)  #b
 	sll $12, $11, 4
 	add $12, $12, $10
-	sw $12, 0xEC70($0)  #show a and b
+	sw $12, 0xDC70($0)  #show a and b
 	j control
 
 signTurn:
